f2c002a3b777aa96603531c029cbd1e8e7bffef1
AleksaMCode <aleksamcode@gmail.com>
2023-05-10 14:58:14 +0200

    nUnit tests added

---
+++ BNFParser.Tests/BnfParserTests.cs
@@ -0,0 +1,27 @@
+﻿using NUnit.Framework;
+using System.IO;
+
+namespace BNFParser.Tests
+{
+    public class BnfParserTests
+    {
+        private string resourcesDirectory;
+
+        [SetUp]
+        public void Setup()
+        {
+            resourcesDirectory = Directory.GetParent(Directory.GetCurrentDirectory()).Parent.FullName + Path.DirectorySeparatorChar + "Resources" + Path.DirectorySeparatorChar;
+        }
+
+        [TestCase("math_addition")]
+        [TestCase("simple_math")]
+        [TestCase("combo")]
+        [TestCase("email_only")]
+        [TestCase("email_only_too_deep_recursive")]
+        [TestCase("math_addition_undefined_nonterminal")]
+        public void BnfParsing(string testCase)
+        {
+            Assert.Pass();
+        }
+    }
+}
