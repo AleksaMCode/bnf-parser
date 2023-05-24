fd3ba55bc3cb813a644c60f9fa12cc1f2f509da9
AleksaMCode <aleksamcode@gmail.com>
2023-05-24 18:59:04 +0200

    Code refactored

--- BNFParser/NonTerminalToken.cs
+++ BNFParser/NonTerminalToken.cs
@@ -1,17 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace BNFParser
+﻿namespace BNFParser
 {
     public class NonTerminalToken : Token
     {
         public static readonly string leftSymbol = "<", rightSymbol = ">";
 
-        public NonTerminalToken() : base() { }
-        public NonTerminalToken(string name) : base(name) { }
+        public NonTerminalToken() : base()
+        {
+        }
+
+        public NonTerminalToken(string name) : base(name)
+        {
+        }
 
         public override int match(string str)
         {
