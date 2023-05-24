a0e3cf583fb39d5b30f314aea6c94925446024ae
AleksaMCode <aleksamcode@gmail.com>
2023-05-24 19:06:42 +0200

    Code refactored

--- BNFParser/Token.cs
+++ BNFParser/Token.cs
@@ -1,37 +1,41 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace BNFParser
 {
     public abstract class Token
     {
-        private string name;
+        private string _name;
 
         public string Name
         {
             get
             {
-                return name;
+                return _name;
             }
             set
-                => name = value;            
+            {
+                _name = value;
+            }
         }
 
         public Token()
-            => name = "";
+        {
+            _name = "";
+        }
 
         public Token(string name)
-            => this.name = name;
+        {
+            _name = name;
+        }
 
         public virtual bool equals(Object o)
         {
             if (o == null || !(o is Token))
+            {
                 return false;
+            }
 
-            return string.Compare(((Token)o).Name, name) == 0;
+            return string.Compare(((Token)o).Name, _name) == 0;
         }
 
         public abstract int match(string str);
