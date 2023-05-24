4697299be807e1cb350438b8fbd3b5c71015e54d
AleksaMCode <aleksamcode@gmail.com>
2023-05-24 19:02:15 +0200

    Code refactored

--- BNFParser/RegexTerminalToken.cs
+++ BNFParser/RegexTerminalToken.cs
@@ -1,44 +1,46 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
 using System.Text.RegularExpressions;
 
 namespace BNFParser
 {
     public class RegexTerminalToken : TerminalToken
     {
-        private readonly Regex rgxPattern;
+        private readonly Regex _rgxPattern;
 
         public Regex RgxPattern
         {
             get
             {
-                return rgxPattern;
+                return _rgxPattern;
             }
         }
 
-        public RegexTerminalToken() : base() { }
+        public RegexTerminalToken() : base()
+        {
+        }
 
         public RegexTerminalToken(string pattern) : base(pattern)
-            => rgxPattern = new Regex(pattern); // ArgumentException & ArgumentNullException     
+        {
+            _rgxPattern = new Regex(pattern); // ArgumentException & ArgumentNullException     
+        }
 
         //public bool isItAMatch(string rgx)
         //    => rgxPattern.IsMatch(rgx);
 
         public override int match(string str)
         {
-            Match match = rgxPattern.Match(str);
+            Match match = _rgxPattern.Match(str);
             if (match.Success)
+            {
                 return /*match.Index +*/ match.Length;
+            }
             return -1;
         }
 
         public List<string> GetCaptureGroups(string rgx)
         {
             List<string> captureGroups = new List<string>();
-            Match match = rgxPattern.Match(rgx);
+            Match match = _rgxPattern.Match(rgx);
             while (match.Success)
             {
                 captureGroups.Add(match.ToString());
@@ -51,7 +53,9 @@ namespace BNFParser
         public override bool equals(object o)
         {
             if (o == null || !(o is RegexTerminalToken))
+            {
                 return false;
+            }
 
             return string.Compare(((RegexTerminalToken)o).RgxPattern.ToString(), RgxPattern.ToString()) == 0;
         }
