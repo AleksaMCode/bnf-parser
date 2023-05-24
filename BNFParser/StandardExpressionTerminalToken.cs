ebc24796eb856bf1bf0124683d8552fbe7d834a2
AleksaMCode <aleksamcode@gmail.com>
2023-05-24 19:04:17 +0200

    Code refactored

--- BNFParser/StandardExpressionTerminalToken.cs
+++ BNFParser/StandardExpressionTerminalToken.cs
@@ -1,44 +1,48 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Text.RegularExpressions;
+﻿using System.Text.RegularExpressions;
 
 namespace BNFParser
 {
     public class StandardExpressionTerminalToken : TerminalToken
     {
-        private readonly Regex rgxPattern;
+        private readonly Regex _rgxPattern;
 
         public Regex RgxPattern
         {
             get
             {
-                return rgxPattern;
+                return _rgxPattern;
             }
         }
 
-        public StandardExpressionTerminalToken() : base() { }
+        public StandardExpressionTerminalToken() : base()
+        {
+        }
 
         public StandardExpressionTerminalToken(string pattern) : base(pattern)
-            => rgxPattern = new Regex(pattern);
+        {
+            _rgxPattern = new Regex(pattern);
+        }
 
         public bool IsItAMatch(string rgx)
-            => rgxPattern.IsMatch(rgx);
+        {
+            return _rgxPattern.IsMatch(rgx);
+        }
 
         public override int match(string str)
         {
-            if (rgxPattern.IsMatch(str))
+            if (_rgxPattern.IsMatch(str))
+            {
                 return str.Length;
+            }
             return -1;
         }
-
 
         public override bool equals(object o)
         {
             if (o == null || !(o is StandardExpressionTerminalToken))
+            {
                 return false;
+            }
 
             return string.Compare(((StandardExpressionTerminalToken)o).RgxPattern.ToString(), RgxPattern.ToString()) == 0;
         }
